6da8545b7bf00bb025c3f48bee4b652849681c17
dipace7 <codeict.dipesh@gmail.com>
2025-03-26 21:10:17 +0545

    add zookeeper

---
+++ src/services/zookeeper.ts
@@ -0,0 +1,22 @@
+import ZooKeeper from "zookeeper";
+
+const host = "https//10.45.3.141:2181";
+
+function createClient(timeoutMs = 5000) {
+  const config = {
+    connect: host,
+    timeout: timeoutMs,
+    debug_level: ZooKeeper.constants.ZOO_LOG_LEVEL_WARN,
+    host_order_deterministic: false,
+  };
+
+  return new ZooKeeper(config);
+}
+
+export const client = createClient();
+
+client.init({});
+
+client.on("connect", () => {
+  console.log("Connected to ZooKeeper");
+});

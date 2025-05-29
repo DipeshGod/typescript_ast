cf9f03c5847ef9faa1e74a8cd0b198a32878ec66
dipace7 <codeict.dipesh@gmail.com>
2025-05-30 02:10:54 +0545

    decouple doc team

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { deviceGroupRouter } from "./routes/configurations/device-groups/route";
 import { client } from "./logpoint/client";
 import "./services/zookeeper";
+import { registeredRoutes } from "./registered-routes";
 
 dotenv.config();
 
@@ -13,14 +14,16 @@ app.use(express.json());
 
 app.use("/configapi/DeviceGroups", deviceGroupRouter);
 
-app.get("/", (req: Request, res: Response) => {
+app.get("/grpc-test", (req: Request, res: Response) => {
   client.SayHello({ name: "Director" }, (error: any, response: any) => {
     if (error) console.error(error);
     else res.send(response.message);
   });
 });
 
-console.log();
+app.get("/registered-routes", (req: Request, res: Response) => {
+  res.status(200).json(registeredRoutes);
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);

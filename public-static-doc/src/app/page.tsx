24e564fcd24eccce72492ff4109bac46ef514c10
dipace7 <codeict.dipesh@gmail.com>
2025-05-30 03:03:10 +0545

    generate static doc

---
+++ public-static-doc/src/app/page.tsx
@@ -0,0 +1,10 @@
+import registeredRoutes from "../../registered-routes.json";
+
+export default function Home() {
+  console.log(registeredRoutes);
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      hello
+    </div>
+  );
+}
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { deviceGroupRouter } from "./routes/configurations/device-groups/route";
 import "./services/zookeeper";
-import registeredRoutes from "../registered-routes.json";
 
 dotenv.config();
 
@@ -12,10 +11,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/configapi/DeviceGroups", deviceGroupRouter);
-
-app.get("/registered-routes", (req: Request, res: Response) => {
-  res.status(200).json(registeredRoutes);
-});
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
